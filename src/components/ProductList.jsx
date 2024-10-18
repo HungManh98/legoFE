fa3f0a0cc5841a5e55f6908422dfcd0bfc15e0f6
1cam2000 <cananhminh@gmail.com>
2024-10-18 08:51:20 +0700

    add title product

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,68 +1,145 @@
 import React from "react";
-import { Card } from "antd";
+import { Button, Card } from "antd";
+import { FaPlay } from "react-icons/fa";
 const { Meta } = Card;
 
 const ProductList = () => {
   const image = "../";
   return (
-    <div className="product-list">
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          />
-        }
-      >
-        <Meta title="product name" description="mo ta san pham ... " />
-      </Card>
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          />
-        }
-      >
-        <Meta title="product name" description="mo ta san pham ... " />
-      </Card>
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          />
-        }
-      >
-        <Meta title="product name" description="mo ta san pham ... " />
-      </Card>
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          />
-        }
-      >
-        <Meta title="product name" description="mo ta san pham ... " />
-      </Card>
-    </div>
+    <>
+      <div className="product-new">
+        <h2>Hàng Mới</h2>
+        <div className="product-list">
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+        </div>
+        <div className="btn">
+          <Button>
+            Xem Thêm <FaPlay size={15} />
+          </Button>
+        </div>
+      </div>
+      <div className="product-new">
+        <h2>Bán Chạy Nhất</h2>
+        <div className="product-list">
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              />
+            }
+          >
+            <Meta title="product name" description="mo ta san pham ... " />
+          </Card>
+        </div>
+        <div className="btn">
+          <Button>
+            Xem Thêm <FaPlay size={15} />
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
