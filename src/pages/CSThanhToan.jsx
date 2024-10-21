1f29147bc080ca6f054e278533903c2b1bd5193d
HungManh98 <95729408+HungManh98@users.noreply.github.com>
2024-10-21 19:29:14 +0700

    Update CSThanhToan.jsx

--- src/pages/CSThanhToan.jsx
+++ src/pages/CSThanhToan.jsx
@@ -13,52 +13,42 @@ const CSThanhToan = () => {
       <div className="chinhSach">
         <h3>Chính sách thanh toán</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          1. Thanh toán khi nhận hàng (COD):
+            Quý khách có thể thanh toán trực tiếp cho nhân viên giao hàng khi nhận hàng.
+            Áp dụng cho các đơn hàng giao trong nội thành và một số tỉnh thành khác theo phạm vi hỗ trợ của đơn vị vận chuyển.
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          2. Thanh toán chuyển khoản ngân hàng:
+            Khách hàng có thể thanh toán trước qua chuyển khoản ngân hàng nội địa.
+            Thông tin tài khoản ngân hàng sẽ được shop cung cấp qua tin nhắn khi xác nhận đơn hàng.
+            Sau khi hoàn tất chuyển khoản, quý khách vui lòng gửi ảnh chụp biên lai hoặc thông báo xác nhận giao
+            dịch thành công để shop xác nhận và tiến hành đóng gói hàng.
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          Lưu ý:
+            Kiểm tra kỹ thông tin chuyển khoản (số tài khoản, nội dung chuyển khoản) để tránh sai sót.
+            Đối với thanh toán chuyển khoản, shop chỉ xử lý đơn hàng sau khi nhận được thanh toán.
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          3. Hoàn tiền
+            Trong trường hợp đơn hàng bị hủy hoặc có sự cố không thể giao hàng, shop sẽ hoàn lại tiền cho khách qua phương thức chuyển khoản.
+            Thời gian hoàn tiền dự kiến: Từ 3-5 ngày làm việc kể từ khi xác nhận hoàn tiền.
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         
         </p>
       </div>
       <Footer />
