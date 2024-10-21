9736cd48ae8126a77c17ea24a76000609f4b1768
HungManh98 <95729408+HungManh98@users.noreply.github.com>
2024-10-21 19:26:49 +0700

    Update CSBaoMat.jsx

--- src/pages/CSBaoMat.jsx
+++ src/pages/CSBaoMat.jsx
@@ -13,10 +13,12 @@ const CSBaoMat = () => {
       <div className="chinhSach">
         <h3>Chính sách bảo mật</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         1. Mục đích thu thập thông tin cá nhân
+         Bạn có thể ghé thăm và duyệt Website của chúng tôi bình thường mà không tiết lộ Thông tin Cá nhân của mình.
+         Tuy nhiên, trong trường hợp bạn muốn mua hàng, bạn cần phải đăng ký: (a) họ và tên của bạn, (b) số điện thoại,
+          (c) địa chỉ thư điện tử, (d) mật khẩu, (e) ngày sinh; (f) địa chỉ nơi cư trú với Website.
+         Mục đích thu nhập thông tin là để chúng tôi có cơ sở để xác nhận đơn hàng của quý vị.
+
         </p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
