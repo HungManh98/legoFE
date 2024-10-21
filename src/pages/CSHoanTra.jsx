bdd0b19235c78382ec757e312b808716085ddd6b
HungManh98 <95729408+HungManh98@users.noreply.github.com>
2024-10-21 19:34:27 +0700

    Update CSHoanTra.jsx

--- src/pages/CSHoanTra.jsx
+++ src/pages/CSHoanTra.jsx
@@ -11,54 +11,52 @@ const CSHoanTra = () => {
         <FixedHeader />
       </div>{" "}
       <div className="chinhSach">
-        <h3>Chính sách thanh toán</h3>
+        <h3>Chính sách hoàn hàng, khiếu nại, bảo hành</h3>
         <br />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         1. Chính sách bảo hành
+           Sản phẩm còn trong thời gian bảo hành được ghi nhận trên hóa đơn mua hàng của webiste Toyriot và được xác định là 
+           lỗi nhà sản xuất. Việc xác định lỗi được thực hiện đơn phương bởi cửa hàng Toyriot.
+           Khách hàng có thể gửi sản phẩm về cửa hàng ghi trên hóa đơn qua đường bưu điện. Chi phí vận chuyển sẽ do khách hàng chi trả.
+           Phía Toyriot sẽ tiến hành kiểm tra tổng quan sản phẩm, nếu phát hiện lỗi hư hỏng do từ phía Khách hàng không phải lỗi kỷ
+           thuật từ nhà sản xuất (Bể, nước vào sản phẩm, ..) thì Toyriot tư vấn cho Khách hàng và từ chối tiếp nhận bảo hành. Nếu KH vẫn mong muốn được hỗ trợ sửa chữa thì tư vấn cho Khách hàng theo Quy trình sửa chữa tính phí.
+           Thời gian bảo hành: 3 ngày kể từ ngày nhận hàng
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          2. Điều kiện được đổi/trả hàng hóa
+            Sản phẩm bị lỗi kỹ thuật do nhà sản xuất hoặc thiếu chi tiết nhưng bên ngoài sản phẩm không bị trầy xước/ bể/ vỡ/ móp méo.
+            Sản phẩm khách hàng nhận không đúng sản phẩm đã đặt hàng và sản phẩm còn bao bì nguyên vẹn, không bị rách/ móp.
+            Sản phẩm có bao bì cũ, bị móp méo, trầy xước không phải do lỗi của khách hàng (ngoại trừ sản phẩm được thông tin là hàng bán thanh lý).
+            Quý khách vui lòng đính kèm video bóc hộp hàng để đối chứng.
+            Thời gian đổi trả trong vòng 03 ngày kể từ ngày khách hàng nhận được hàng hóa (thời gian này căn cứ thời gian khách hàng ký nhận trên vận đơn giao hàng).
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          3. Chính sách đổi hàng
+            Đơn hàng thanh toán COD/Chuyển khoản
+            Hình thức hoàn tiền: chuyển khoản cho các đơn hàng thanh toán COD/Chuyển khoản.
+            Thời gian chuyển khoản trong vòng 7 ngày làm việc kể từ ngày cửa hàng Toyriot nhận được hàng hóa khách hàng gửi lại.
+            Cách thức gửi trả hàng:
+            Bước 1: Khách hàng chụp hình/ quay clip sản phẩm bị lỗi hoặc bị hư hỏng… gửi thông tin và yêu cầu trả hàng qua facebook
+            facebook.com/toyriotstore hoặc website toyriot.net
+            Bước 2: Khách hàng gửi trả hàng theo đến địa chỉ của Toyriot
+            Địa chỉ: 18 Tam Trinh, P. Minh Khai, Q. Hai Bà Trưng, Hà Nội
+
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         
         </p><p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+        
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+          
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi
-          iste quibusdam. Aut quam nostrum distinctio? Quos quidem a veniam
-          facilis alias ipsum hic fugiat molestiae obcaecati, doloremque dolorem
-          odit.
+         
         </p>
       </div>
       <Footer />
