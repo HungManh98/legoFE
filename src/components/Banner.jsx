5c5f720854a466bd4e2ae1662a7a065274d94f4b
1cam2000 <cananhminh@gmail.com>
2024-10-25 01:03:04 +0700

    fix confict

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { Carousel } from "antd";
 
-const contentStyle = {
-  margin: 0,
-  height: "460px",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
-
 const Banner = () => {
   const onChange = (currentSlide) => {
     console.log(currentSlide);
@@ -19,38 +11,30 @@ const Banner = () => {
       <Carousel autoplay afterChange={onChange}>
         <div>
           <img
-            style={contentStyle}
+            style={{ width: "100%", height: "500px" }}
             src="/images/img-banner.jpg"
             alt="Slide 1"
-            width="100%"
-            height="480px"
           />
         </div>
         <div>
           <img
-            style={contentStyle}
+            style={{ width: "100%", height: "500px" }}
             src="/images/img-banner.jpg"
             alt="Slide 2"
-            width="100%"
-            height="460px"
           />
         </div>
         <div>
           <img
-            style={contentStyle}
+            style={{ width: "100%", height: "500px" }}
             src="/images/img-banner.jpg"
             alt="Slide 3"
-            width="100%"
-            height="460px"
           />
         </div>
         <div>
           <img
-            style={contentStyle}
+            style={{ width: "100%", height: "500px" }}
             src="/images/img-banner.jpg"
             alt="Slide 4"
-            width="100%"
-            height="460px"
           />
         </div>
       </Carousel>
